822dbba1c7cac602c6a038741fd0d2f02b387082
Lucas Dantas <lucas.dts98@gmail.com>
2023-03-06 11:46:58 -0300

    recapitulando

--- fundamentos/JS-primeiros-passos/scripts.js
+++ fundamentos/JS-primeiros-passos/scripts.js
@@ -1,39 +1,100 @@
-let currentHour = 10;
-let message = "";
-if (currentHour >= 22) {
-    message = "Não deveríamos comer nada, é hora de dormir";
-} else if (currentHour >= 18 && currentHour < 22) {
-    message = "Rango da noite, vamos jantar :D";
-} else if (currentHour >= 14 && currentHour < 18) {
-    message = "Vamos fazer um bolo pro café da tarde?";
-} else if (currentHour >= 11 && currentHour < 14) {
-    message = "Hora do almoço!!!";
-} else if (currentHour >= 4 && currentHour < 11) {
-    message = "Hmmm, cheiro de café recém-passado";
-}
+// let currentHour = 10;
+// let message = "";
+// if (currentHour >= 22) {
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     message = "Rango da noite, vamos jantar :D";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// } else if (currentHour >= 11 && currentHour < 14) {
+//     message = "Hora do almoço!!!";
+// } else if (currentHour >= 4 && currentHour < 11) {
+//     message = "Hmmm, cheiro de café recém-passado";
+// }
 
-console.log(message);
+// console.log(message);
 
 
-let weekDay = "quarta-feira";
-if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
-} else {
-    console.log("FINALMENTE, descanso merecido! UwU!");
-}
+// let weekDay = "quarta-feira";
+// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// } else {
+//     console.log("FINALMENTE, descanso merecido! UwU!");
+// }
 
-let posicao = "lista";
-switch (posicao) {
-    case "aprovada":
-        console.log("Parabéns, você está no grupo de pessoas aprovadas!");
-        break
-    case "lista":
-        console.log("Você está na nossa lista de espera.");
-        break
-    case "reprovada":
-        console.log("Infelizmente, você reprovou.");
-        break
-        default:
-            console.log("Informação incorreta.");
-            break
-} 
+// let posicao = "lista";
+// switch (posicao) {
+//     case "aprovada":
+//         console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+//         break
+//     case "lista":
+//         console.log("Você está na nossa lista de espera.");
+//         break
+//     case "reprovada":
+//         console.log("Infelizmente, você reprovou.");
+//         break
+//         default:
+//             console.log("Informação incorreta.");
+//             break
+// } 
+
+// ================================================================================
+
+// const age = 20;
+// let usuario = 'Lucas';
+
+// console.log(age);
+// console.log(usuario);
+
+// =======================================
+
+// let myName = 'Lucas';
+// let birthCity = 'Joao Pessoa';
+// let birthYear = 1998;
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// ====================================================
+
+// let movie = 'Avengers'; // String
+// let score = 10; // Number
+// let isValide = true; // Boolean
+// let usuario; // Undefined
+// let color = null; // Valor nulo - redefinicao!
+
+// ===================================================
+
+// let salary = 3500;
+// salary += 1
+
+// // console.log(salary + salary);
+// // console.log(salary - salary);
+// // console.log(salary * salary);
+// // console.log(salary / salary);
+// // console.log(salary ** salary);
+
+// console.log(salary);
+
+// ================================================
+
+// let patientId = 50;
+// let isEnrolled = true;
+// let patientInfo = {
+//     firstName: 'Lucas',
+//     lastName: 'Dantas',
+// };
+// let patientEmail = 'lucas.dts98@gmail.com';
+
+// ================================================
+
+// let base = 5;
+// let heigth = 8;
+// let area = base * heigth;
+// let perimeter = 5 + 5 + 8 + 8;
+
+// console.log(area);
+// console.log(perimeter);
+
+// ===============================================
